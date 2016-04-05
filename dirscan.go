22641129ae01c2478dc3c7e0850f8ac30ab6c93c
Reynold Chery <joshbaptiste@yahoo.com>
2016-04-05 18:52:46 -0400

    Scaffolding

--- dirscan.go
+++ dirscan.go
@@ -1,9 +1,21 @@
 package client
 
 import (
-    "fmt"
+    "os"
+    "github.com/iamthemuffinman/logsip"
 )
 
+var logger = logsip.New(os.Stdout)
 //recursively search directories for matching media files
-func (string dir) ScanDirectory  (map dirs) {
+func ScanDirectory (string dir) map[] {
+    if _, err := os.Stat(dir); err != nil {
+        
+    }
+   else {
+    logger.ERROR(dir, "does not exist")
+    }
+}
+
+func MatchMediaFile(string dir) {
+
 }
