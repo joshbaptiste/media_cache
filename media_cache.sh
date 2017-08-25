b42d6e4cfb6c776d9ea525ec56c46c5f3f90e3bf
rchery <jfltech@gmail.com>
2017-08-25 02:29:37 +0000

    updates

--- media_cache.sh
+++ media_cache.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-script=$(basename $0)
+declare -r SCRIPT=$(basename $0)
 declare -r MAX_CACHE=80000000 #80GB in KB 
 declare -r MIN_SPACE=2000000  #2GB space in KB
 declare -r CACHE_DIR=/mnt/superfree/media/cache
+declare -r DIR_ROOT=/mnt/superfree/media
 declare -r TV_SERIES=/mnt/superfree/media/tv_series
 declare -r FILMS=/mnt/superfree/media/films
 
@@ -12,18 +13,31 @@ function check_running(){
 }
 
 function check_space() {
-    local current_space=$(df -kP "$dir")
+    local current_space=$(df -k  --output=avail "$dir" | awk 'NR==2')
+    if [[ $current_space -lt $MIN_SPACE ]]
+    then
+        remove_local_files
+        remove_local_dirs
+        exit 3
+    fi
 }
 
-function create_links(){
-    while read line
+function remove_local_dirs() {
+    find $CACHE_DIR -type d  -mtime +30 -exec rmdir --ignore-fail-on-non-empty "{}" \;
+}
+
+function remove_local_files() {
+    find $CACHE_DIR -type f  -mtime +30 -exec rm -f "{}" \;
+}
+
+function copy_tvcache_files(){
+    while IFS= read -r -d '' line
     do
-        if [[ ! -e "$CACHE_DIR/$line" ]] 
+        if [[ ! -e "$CACHE_DIR/$line" ]]
         then
-            ln -s "$line" "$CACHE_DIR/$line" 
+            find $TV_SERIES -maxdepth 1 -exec cp --no-clobber -as "tv_series/$line" "$CACHE_DIR/$line"
         fi
-    done < <(find $TV_SERIES $FILMS)
+    done < <(find $TV_SERIES -print0 )
 }
 
-funct
 
